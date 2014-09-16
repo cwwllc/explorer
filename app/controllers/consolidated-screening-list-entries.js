597b4ac42664dfbcf8e4a2f81a3c98190400daa7
Calum Halcrow <calumhalcrow@gmail.com>
2014-09-16 11:14:39 +0800

    Full country list.

--- app/controllers/consolidated-screening-list-entries.js
+++ app/controllers/consolidated-screening-list-entries.js
@@ -2,24 +2,42 @@ import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
   queryParams: ['q', 'countries', 'sources', 'sdnType', 'page'],
-  q: null,
-  countries: null,
-  sources: null,
-  sdnType: null,
-  page: null,
 
+  q: null,
   qField: Ember.computed.oneWay('q'),
-  countriesField: Ember.computed.oneWay('countries'),
+
+  // The mapping between the countries-field component and the controller is
+  // more complex than that of the other params, due to the select box's value
+  // and option text being different. Both "countries" and "countriesField"
+  // are bound to the component. "countries" gives the component the query
+  // string value, for setting selected options initally, and "countriesField"
+  // gives the selected option back to the controller, as an array of Objects
+  // rather than as a comma-separated string of values.
+  //
+  // This seems more complex than it should be, which suggests there may be a
+  // better way.
+  countries: null,
+  countriesField: null,
+
+  sources: null,
   sourcesField: Ember.computed.oneWay('sources'),
+
+  sdnType: null,
   sdnTypeField: Ember.computed.oneWay('sdnType'),
+
+  page: null,
   pageField: Ember.computed.oneWay('pageField'),
 
   actions: {
     search: function(page) {
       this.set('q', this.get('qField'));
-      this.set('countries', this.get('countriesField'));
       this.set('sources', this.get('sourcesField'));
       this.set('sdnType', this.get('sdnTypeField'));
+
+      this.set('countries', this.get('countriesField').map(function(item) {
+        return item.value;
+      }));
+
       this.set('page', (page || 1));
     }
   },
