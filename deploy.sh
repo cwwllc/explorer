5f68a61a43ef37631b6e63a2e66f7ed9bb90eecb
Calum Halcrow <calumhalcrow@gmail.com>
2014-10-16 18:59:49 +0800

    Better deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,22 @@
-if [ ! -d "deploy" ]; then
-    git clone git@github.com:GovWizely/explorer.git deploy
-fi
+#!/usr/bin/env bash
 
-cd deploy
-git checkout gh-pages
-git pull
-cd ..
+# Taken from:
+# https://github.com/nathanhammond/ember-flows-generator/blob/master/deploy.sh
 
+# Make sure we're in a good state.
+git checkout master
+git pull --rebase
+rm -rf dist
 ember build --environment production
-cd dist
-cp -r `ls -1 | grep -v -E 'test'` ../deploy
 
-cd ../deploy
-git add .
-git commit -m 'Updated.'
+# Move to the "release" branch and make everything work.
+git checkout gh-pages
+git pull --rebase
+ls -1 | grep -v -E '^dist|bower_components|node_modules|tmp$' | xargs rm -rf
+mv dist/* ./
+rmdir dist
+
+# Add everything and push it.
+git commit -am "Update."
 git push origin gh-pages
+git checkout master
