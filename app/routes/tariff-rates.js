63f53ed7e6cdc0c05e2cffc09de24a68979568f9
James Isaacs <jamesiisaacs@gmail.com>
2014-10-30 14:27:22 -0700

    Remove countries from explorer for tariff rates

--- app/routes/tariff-rates.js
+++ app/routes/tariff-rates.js
@@ -3,24 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   queryParams: {
     q: { refreshModel: true },
-    countries: { refreshModel: true },
     sources: { refreshModel: true },
     page: { refreshModel: true }
   },
 
   actions: {
     search: function(page) {
-      var countries,
-        sources,
-        countriesField = this.controller.get('countriesField'),
+      var sources,
         sourcesField = this.controller.get('sourcesField');
-
-      if (countriesField) {
-        countries = countriesField.map(function(item) {
-          return item.value;
-        });
-      }
-      this.controller.set('countries', countries);
 
       if (sourcesField) {
         sources = sourcesField.map(function(item) {
